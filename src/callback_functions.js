3a50f6489c0f6fdc866367ae4d33c1b9ba07ea2d
Christopher Stokley <christopher.stokley@student.swic.edu>
2025-03-20 08:56:42 -0500

    Showed function in variables and used arrow functions.

---
+++ src/callback_functions.js
@@ -0,0 +1,12 @@
+const greeting = function() {
+  return 'Hello World'}// variable storing a function
+
+const greet = () => {
+  return 'Hello World'}// arrow function doing the same thing as the function above
+
+const hello = () => 'Hello World'// arrow function that was shortened to one line
+
+console.log(greeting())// prints 'Hello World'
+console.log(greet())// prints 'Hello World'
+console.log(hello())// prints 'Hello World'
+
